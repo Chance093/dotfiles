0dfcfc08d6441b5a717aef6d609514c5399e9504
Chance093 <chance.dev093@gmail.com>
2026-03-08 16:05:29 -0700

    add homebrew installation

--- install.sh
+++ install.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
 
+install_linux() {
+  echo "You are in linux"
+}
+
+install_macos() {
+  # install homebrew
+  if [[ "$(brew --version)" == *"command not found"* ]]; then 
+    echo "Installing homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+
+  # install packages in brew and brew cask files
+
+  # Create symlinks
+}
+
 # detect OS
+case $(uname -s) in 
+  Linux) install_linux;;
+  Darwin) install_macos;;
+  *) echo "Don't know what this is";;
+esac
+
 # Install package managers depending on OS
 # Install packages based on appropriate files
 # Install zsh and set as default shell (possibly)
