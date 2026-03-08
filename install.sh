ae608419fd47c2a7fc32d111150e95a924801f6b
Chance093 <chance.dev093@gmail.com>
2026-03-08 15:05:40 -0700

    touch install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# detect OS
+# Install package managers depending on OS
+# Install packages based on appropriate files
+# Install zsh and set as default shell (possibly)
+# Create symlinks for all configs
+# Open nvim headlessly so lazy.nvim installs all plugins
+# Copy secrets.example.sh => secrets.sh if it doesn't exist
+# Print a summary of what has been done
