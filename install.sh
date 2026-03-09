ef8f712f2692961c826e0ac9afdfc79c45b3e788
Chance093 <chance.dev093@gmail.com>
2026-03-08 19:09:11 -0700

    add package downloading with brew

--- install.sh
+++ install.sh
@@ -1,32 +1,34 @@
 #!/usr/bin/env bash
 
-install_linux() {
-  echo "You are in linux"
-}
+BREW_PACKAGE_FILE="packages/brew.txt"
 
 install_macos() {
+  # check for OS
+  if [[ "$(uname -s)" != "Darwin" ]]; then
+    echo "Error: Must be on macOS to run this script." >&2
+    exit 1
+  fi
+  
   # install homebrew
   if [[ "$(brew --version)" == *"command not found"* ]]; then 
     echo "Installing homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 
-  # install packages in brew and brew cask files
+  # install packages in brew.txt
+  if [[ -f "$BREW_PACKAGE_FILE" ]]; then
+    while IFS= read -r line <&3; do
+      brew install "$line"
+    done 3< "$BREW_PACKAGE_FILE"
+  else
+    echo "Error: Input file '$BREW_PACKAGE_FILE' not found." >&2
+    exit 1
+  fi
 
   # Create symlinks
+  # Open nvim headlessly so lazy.nvim installs all plugins
+  # Copy secrets.example.sh => secrets.sh if it doesn't exist
+  # Print a summary of what has been done
 }
 
-# detect OS
-case $(uname -s) in 
-  Linux) install_linux;;
-  Darwin) install_macos;;
-  *) echo "Don't know what this is";;
-esac
-
-# Install package managers depending on OS
-# Install packages based on appropriate files
-# Install zsh and set as default shell (possibly)
-# Create symlinks for all configs
-# Open nvim headlessly so lazy.nvim installs all plugins
-# Copy secrets.example.sh => secrets.sh if it doesn't exist
-# Print a summary of what has been done
+install_macos
